9187748f5a26c4a03484073b802dab12b25d44fa
AhmedMostafa30 <ahmocs30@gmail.com>
2024-10-10 04:55:46 +0300

    disable ssr for loading md editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,7 +4,7 @@ import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
 import { useState } from "react";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -12,6 +12,9 @@ import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
